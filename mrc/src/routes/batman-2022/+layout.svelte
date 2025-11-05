cbd09a704f45fa29ba64da0dfc655865e9772c3b
Sidopjescherm <hermussidney@gmail.com>
2025-11-05 15:24:18 +0100

    #9 Layout
    The navigation is now in the layout as it should be on every page that is to come

---
+++ mrc/src/routes/batman-2022/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    /** @type {import('./$types').LayoutProps} */
+    let { data, children } = $props();
+    import Navigation from '$lib/components/Navigation.svelte';
+</script>
+<Navigation />
+
+{@render children()}
