719f9dd063d6a62434ab71333b6ff7241ea5d1ee
Sidopjescherm <hermussidney@gmail.com>
2025-11-05 16:11:03 +0100

    #11 Small screen menu
    The mobile menu has been added. I will enhance this further once I am done with the large screen navigation.

--- mrc/src/lib/components/Navigation.svelte
+++ mrc/src/lib/components/Navigation.svelte
@@ -1,23 +1,62 @@
-<ul>
-    <li><a href="#batman">BATMAN</a></li>
-    <li><a href="#characters">CHARACTERS</a></li>
-    <li><a href="#vehicles">VEHICLES</a></li>
-    <li><a href="#city">CITY</a></li>
-    <li><a href="#story">STORY</a></li>
-</ul>
+<header>
+    <a href="#menu" class="menu-toggle">MENU</a>
+    <nav id="menu">
+        <a href="#" class="menu-close">CLOSE</a>
+        <ul>
+            <li><a href="#batman">BATMAN</a></li>
+            <li><a href="#characters">CHARACTERS</a></li>
+            <li><a href="#vehicles">VEHICLES</a></li>
+            <li><a href="#city">CITY</a></li>
+            <li><a href="#story">STORY</a></li>
+        </ul>
+    </nav>
+</header>
 
 <style>
-    ul {
-        list-style: none;
-
-        display: flex;
-        justify-content: center;
-        gap: 1rem;
+    header {
+        padding: 0.5em;
+        position: sticky;
+        top: 0;
+        z-index: 100;
 
         background-color: var(--accent-color-yellow);
         @media (prefers-color-scheme: dark) {
             background-color: var(--neutral-color-darkest);
         }
+
+        border-bottom: 0.25rem solid var(--primary-color-red);
+    }
+    nav {
+        position: fixed;
+        top: 0;
+        right: -100%;
+        width: 100%;
+        transition: right 0.3s ease;
+        display: flex;
+        flex-direction: column;
+        padding: 1em;
+
+        background-color: var(--accent-color-yellow);
+        @media (prefers-color-scheme: dark) {
+            background-color: var(--neutral-color-darkest);
+        }
+
+        border-bottom: 0.25rem solid var(--primary-color-red);
+    }
+
+    #menu:target {
+        right: 0;
+    }
+
+    .menu-close {
+        position: absolute;
+        top: 1em;
+        right: 1.2em;
+    }
+    
+    ul {
+        list-style: none;
+        gap: 1rem;
     }
 
     a{
@@ -26,5 +65,9 @@
             color: var(--primary-color-red);
         }
         text-decoration: none;
+        background-color: var(--accent-color-yellow);
+        @media (prefers-color-scheme: dark){
+            background-color: var(--neutral-color-darkest);
+        }
     }
 </style>
