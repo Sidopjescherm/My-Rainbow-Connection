0ab45c88277a8f2c40599be5108a061c4b267a98
Sidopjescherm <hermussidney@gmail.com>
2026-06-16 23:25:49 +0200

    yeh

--- mrc/src/routes/+page.svelte
+++ mrc/src/routes/+page.svelte
@@ -5,9 +5,19 @@
         <h2>Greetings webtraveler!</h2>
 
         <p>
-            My name is Sidney and I welcome you to my page. This is a project where I'll find the raibow connection 
+            My name is Sidney and I welcome you to my page. This is a project where I'll find the rainbow connection 
             that Kermit sings of. For me that means seeing what frontend means for me. Now pull up a chair and see
             what this site has to offer.
         </p>
+
+        <picture>
+            <img src="" alt=""/>
+        </picture>
     </article>
 </main>
+
+<style>
+    p {
+        max-width: 80ch;
+    }
+</style>
